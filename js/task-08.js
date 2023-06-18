baac3c81f7cfe1d82dd3339c4ce482043b3dad66
Oleksii Stepchuk <34480855+sirLightRain@users.noreply.github.com>
2023-06-18 16:06:21 +0300

    + task till 9 including

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const emailInput = loginForm.elements.email;
+  const passwordInput = loginForm.elements.password;
+
+  if (emailInput.value === "" || passwordInput.value === "") {
+    alert("Заповніть всі форми!");
+    return;
+  }
+
+  const formData = {
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+
+  console.log(formData);
+  loginForm.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const widget = document.querySelector(".widget");
+const coloredText = document.querySelector(".color");
+const changeColorBtn = document.querySelector(".change-color");
+
+changeColorBtn.addEventListener("click", handlerChangeColor);
+
+function handlerChangeColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  coloredText.textContent = randomColor;
+}
